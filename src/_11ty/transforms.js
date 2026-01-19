ebf94cbe7ba55eab4ac56c684065ad418b4c9ea5
polent <github@polente.de>
2026-01-19 14:03:47 +0100

    updated vulnerabilities and 11ty versions

--- src/_11ty/transforms.js
+++ src/_11ty/transforms.js
@@ -1,17 +1,10 @@
-const htmlMinifier = require("html-minifier");
+// Note: html-minifier@4.0.0 has a known REDoS vulnerability with no fix available
+// Using PostCSS for CSS minification and basic HTML output instead
+// const htmlMinifier = require("html-minifier");
 
 const htmlmin = function (content) {
-	if (this.page.outputPath && this.page.outputPath.endsWith(".html")) {
-		let minified = htmlMinifier.minify(content, {
-			minifyJS: true,
-			minifyCSS: true,
-			removeComments: true,
-			preserveLineBreaks: true,
-			collapseWhitespace: true,
-			removeEmptyElements: true,
-		});
-		return minified;
-	}
+	// HTML minification disabled due to security vulnerability in html-minifier
+	// PostCSS handles CSS minification via postcss.config.js
 	return content;
 };
 
